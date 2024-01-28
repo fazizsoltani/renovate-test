a09531978ce1c0c70641b5e1d2bf12568440fd77
fazizsoltani <farzad.azizsoltani98@gmail.com>
2024-01-28 21:57:19 +0100

    update

---
+++ example/renovate-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  username: 'renovate-release',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  onboarding: true,
+  platform: 'github',
+  includeForks: true,
+  dryRun: 'full',
+  repositories: ['fazizsoltani/renovate-test'],
+
+};
